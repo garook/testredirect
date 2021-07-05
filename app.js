72115b831dfeb7ffc05fb1fcf8fb391ffd5bdbd3
Jeffrey gan <ganecj@gmail.com>
2021-07-05 14:57:10 +0800

    added different routes added pm2 for deployment

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ping", (req, res) => res.end());
+app.get("/ping", ({res}) => res.end());
 
-app.get("*", async (req, res) => {
-  const url = `${req.protocol}://${req.hostname}${req.originalUrl}`;
+app.get("/homehospital", async ({res}) => {
   try {
     return res.redirect("https://form.jotform.com/211774478626062");
   } catch (e) {
-    console.log(`Sending 400 for`, url, e.toString());
+    console.log(`error: `, e.toString());
+    return res.status(400).end();
+  }
+});
+
+app.get("/aia", async ({res}) => {
+  try {
+    return res.redirect("https://form.jotform.com/211775297237060");
+  } catch (e) {
+    console.log(`error: `, e.toString());
     return res.status(400).end();
   }
 });
